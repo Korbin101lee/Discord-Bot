f5024253f09342a97f130529bf376cf6ee7adf8e
Korbin101lee <Khillan5223@gmail.com>
2021-09-15 00:11:09 -0700

    sadsa

--- lib/cogs/welcome.py
+++ lib/cogs/welcome.py
@@ -18,11 +18,12 @@ class Welcome(Cog):
     @Cog.listener()
     async def on_member_join(self, member):
         db.execute("INSERT INTO exp (UserID) VALUES (?)", member.id)
+        await member.add_roles(member.guild.get_role(789324989027319838))
+
         await self.bot.get_channel(725207506040913967).send(f"**IF YOU RECEIVED A DM FROM ALTIDENTIFIER, PLEASE ANSWER THAT FIRST** \n\nHey {member.mention}, welcome to Pro-Life! Before you get access to the rest of the channels, we would like you to answer a few questions. Please post them below and wait for an admin to approve you, which will happen within 12 hours, depending on your timezone. Please read the <#725187690554720328> as well. \n\n**DO NOT PING ADMINS OR OWNERS!**\n\nQuestions:\n1-Are you Pro-Life?\n2-If yes to #1, what exceptions may you consider (life of the mother, rape, etc.)?\n3-Where did you learn of this server?\n4-Do you agree to the rules?\n\nHave a good time!")
         
 
 
-        await member.add_roles(member.guild.get_role(789324989027319838))
 
         #await member.edit(roles=[*member.roles, *[member.guild.get_role(id_) for id_ in (829881736875474974, 829881823634522112)]])
 
